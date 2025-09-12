b921369d47bdb859117eca8fa06d5b6cae51baa8
knowledge-corner <vaidehi.nair@knowledgecorner.in>
2025-09-12 19:06:40 +0530

    Day - 10

---
+++ Classwork/tutorial-04/app_langgraph.py
@@ -0,0 +1,62 @@
+from dotenv import load_dotenv
+import os
+from langgraph.graph import StateGraph, END
+from langchain_openai import ChatOpenAI
+from app_tools import get_shipment_details_tool
+
+load_dotenv()
+
+# Define LLM
+llm = ChatOpenAI(
+    model=os.getenv("MODEL"), 
+    openai_api_key=os.getenv("API_KEY")
+)
+
+# Define States
+class Consignement(dict):
+    consignment_id: str
+    status: str
+    message: str
+
+# Function Node or Tool Node
+def get_shipment_details(consignment: Consignement) -> Consignement:
+    """Fetch shipment details by consignment ID from a JSON API."""
+
+    consignment_id = consignment['consignment_id']
+    result = get_shipment_details_tool(consignment_id)
+    consignment['status'] = result.get('status', 'Unknown') 
+    return consignment  
+
+# LLM Node
+def summarize_status(consignment: Consignement) -> Consignement:
+    """Summarize the shipment status using LLM."""
+
+    prompt = f"Summarize the following shipment status as a message: {consignment['status']}"
+    summary = llm.invoke(prompt)
+    consignment['message'] = [summary]
+
+    return consignment
+
+# Build Graph
+
+graph = StateGraph(Consignement)
+
+# add nodes
+graph.add_node("GetShipmentDetails", get_shipment_details)
+graph.add_node("SummarizeStatus", summarize_status)
+
+# add edges
+graph.set_entry_point("GetShipmentDetails")
+graph.add_edge("GetShipmentDetails", "SummarizeStatus")
+graph.add_edge("SummarizeStatus", END)
+
+# Compile the graph
+app = graph.compile()
+
+# Run the graph - main
+if __name__ == "__main__":
+    consignment_id = "JAI-00001"
+    initial_state = Consignement(consignment_id=consignment_id, status="", message="")
+    final_state = app.invoke(initial_state)
+    print(final_state["message"])
+
--- Classwork/tutorial-04/app_tools.py
+++ Classwork/tutorial-04/app_tools.py
@@ -1,4 +1,4 @@
-from crewai.tools import tool
+# from crewai.tools import tool
 import requests
 from dotenv import load_dotenv
 import os
@@ -8,8 +8,8 @@ load_dotenv()
 
 BASE_URL = os.getenv("BASE_URL")
 
-@tool("Fetch Shipment Details")
-def get_shipment_details_tool(consignment_id : str) -> dict:
+# @tool("Fetch Shipment Details")
+def get_shipment_details_tool(consignment_id):
     """Fetch shipment details by consignment ID from a JSON API."""
 
     response = requests.get(rf"{BASE_URL}/api/shipments/{consignment_id}")
